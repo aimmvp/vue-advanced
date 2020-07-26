0ca3266aeedabed48921d663657f5b98aef14bdf
s0wnd <aimmvp@gmail.com>
2020-07-26 23:05:20 +0900

    공통 컴포넌트 구현(4) 까지

---
+++ vue-news/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <tool-bar></tool-bar>
+    <transition name="page">    
+      <router-view></router-view>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar.vue'
+export default {
+  components: {
+    ToolBar,
+  },
+  methods: {
+    fetchData() { 
+      console.log('hello')
+    }
+  }
+}
+</script>
+<style>
+body {
+  padding:0;
+  margin:0;
+}
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-active {
+  text-decoration: underline;
+
+}
+
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
+}
+</style>
